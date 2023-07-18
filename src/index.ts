69cb39e183ccd0205ba7afa9753e05fe411fc41d
Jason Louie <jason.jw.louie@gmail.com>
2023-07-18 03:12:34 -0700

    chore: update imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Uploader from './components/Uploader/index'
+import MultiUploader from './components/MultiUploader';
 import nextUpload from './next/nextUpload';
 
-export {nextUpload, Uploader};
+export {nextUpload, Uploader, MultiUploader};
